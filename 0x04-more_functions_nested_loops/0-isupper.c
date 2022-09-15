a34866fc08366dea44bc2c4a53524d142355197c
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-15 12:57:13 +0100

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _issupper - checks for uppercase
+ * @c: variable
+ * Return: 1 if funct works else return 0
+*/
+int _isupper(int c)
+{
+if (c == 'A' && c <= 'Z')
+{
+return ('1');
+}
+else
+{
+return ('0');
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+readme for the project 0x04-more_functions_nested_loops , the directory contains my work on c under the alx program
+the topics of c involved: functions, if ,else, for, while etc
