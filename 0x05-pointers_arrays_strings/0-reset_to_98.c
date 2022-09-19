1b5a2e9cc65f07bdc302776df9288ec07dc7b668
emmanuel <attueyiemmanuel2322@gmail.com>
2022-09-19 21:40:50 +0100

    once

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *reset_to_98 - changes the pointed value
+ *@n: variable
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+#define main_h_
+int _putchar(char c);
+void reset_to_98(int *n);
